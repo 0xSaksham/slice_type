7f2440253cf096f38de4d42a83a08fe9755ee583
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-05 16:48:35 +0530

    Fix bug in login functionality

---
+++ target/debug/deps/slice_type-e2e98c564bc3223d.d
@@ -0,0 +1,5 @@
+/home/saksham/Documents/Development/Rust/slice_type/target/debug/deps/slice_type-e2e98c564bc3223d.rmeta: src/main.rs
+
+/home/saksham/Documents/Development/Rust/slice_type/target/debug/deps/slice_type-e2e98c564bc3223d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/slice_type-f095decc43db4d07.d
@@ -0,0 +1,5 @@
+/home/saksham/Documents/Development/Rust/slice_type/target/debug/deps/slice_type-f095decc43db4d07.rmeta: src/main.rs
+
+/home/saksham/Documents/Development/Rust/slice_type/target/debug/deps/slice_type-f095decc43db4d07.d: src/main.rs
+
+src/main.rs:
